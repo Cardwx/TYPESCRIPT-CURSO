0b4c8f5bed8f0ff76a91873a386c19f87d5e7987
Lucas <lcb.barbeiro@Gmail.com>
2023-01-27 21:56:33 -0300

    introdução ao curso

---
+++ 1_INTRO/index.js
@@ -0,0 +1,7 @@
+var fisrtName = "Lucas";
+var anotherName = 1;
+var x = true;
+function greeting(name) {
+    console.log("Olá " + name);
+}
+greeting(fisrtName);
---
+++ 1_INTRO/index.ts
@@ -0,0 +1,8 @@
+const fisrtName = "Lucas"
+const anotherName = 1
+const x = true
+
+function greeting(name: String){
+    console.log("Olá " + name)
+}
+greeting(fisrtName)
