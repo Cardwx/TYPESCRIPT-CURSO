0b0f90c6af35a74fd8afc9422e3401da15428bb2
Lucas <lcb.barbeiro@Gmail.com>
2023-01-27 23:05:10 -0300

    aprofundando no curso

---
+++ 2_FUNDAMENTOS/index.ts
@@ -0,0 +1,50 @@
+// 1 - numbers
+let x: number = 10
+
+console.log(x)
+
+x = 16
+
+console.log(typeof x)
+
+const y: number = 15.788787
+
+console.log(typeof y)
+
+console.log(y)
+
+console.log(y.toPrecision(3))
+
+// 2 - string
+const firstName: string = "Lucas"
+
+console.log(firstName.toUpperCase())
+
+let fullName: string
+
+const lastName: string = "Cardoso"
+
+fullName = firstName + " " + lastName
+
+console.log(fullName)
+
+console.log(typeof fullName)
+
+// 3 - boolean
+
+let a: boolean = false
+
+console.log(a)
+console.log(typeof(a))
+
+a = true
+
+console.log(a)
+
+// 4 - ineference e annotation
+let ann: string = "teste"
+
+let inf = "Teste"
+
+// inf = 1
+// ann = 1
